c95b55aa4f22440cc17f7b0337c150b08da12323
dan gullis <daniel.gullis1@gmail.com>
2024-02-28 15:47:22 +0000

    update bio backend route

--- backend/modules/users.py
+++ backend/modules/users.py
@@ -1,3 +1,17 @@
 from db.mongodb import get_users_collection
 
 #create functions in here to add users / update users / delete users etc
+
+def update_bio(userId, bioContent):
+    users_collection = get_users_collection()
+
+    try:
+        filter_criteria = {"_id": userId}  
+        update_data = {"$set": {"bio": bioContent}} 
+        result = users_collection.update_one(filter_criteria, update_data)
+        
+        return result
+
+    except Exception as e:
+        return e
+
