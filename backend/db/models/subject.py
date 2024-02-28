a27761ab317a27798391feb23c64f5f0ab4d709e
dan gullis <daniel.gullis1@gmail.com>
2024-02-28 14:44:29 +0000

    removed typo

--- backend/db/models/subject.py
+++ backend/db/models/subject.py
@@ -1,6 +1,6 @@
 
 class Subject:
-    def __init__(self, name, gcse, alevelgit):
+    def __init__(self, name, gcse, alevel):
         self.name = name   
         self.gcse = gcse
         self.alevel = alevel 
