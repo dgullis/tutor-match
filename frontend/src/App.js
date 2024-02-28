94ac2760c3a8bd9b21274a0bc1aabd3acb6bf035
dan gullis <daniel.gullis1@gmail.com>
2024-02-28 12:50:25 +0000

    layout component deleted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import { useNavigate, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
-import Layout from "./components/Layout";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Profile from "./components/Profile";
@@ -8,14 +7,12 @@ import Profile from "./components/Profile";
 function App() {
   return (
     <Routes>
-      <Route path = "/" element = { <Layout></Layout> }>
-        <Route index element = { <Login></Login> }></Route>
-        <Route path = "/signup" element = { <Signup></Signup> } ></Route>
-        <Route path = "/profile" element = { <Profile></Profile> }></Route>
-      </Route>
+      <Route path = "/login" element= { <Login /> }></Route>
+      <Route path = "/signup" element = { <Signup /> } ></Route>
+      <Route path = "/profile" element = { <Profile /> }></Route>
       <Route path='/landing' element={<LandingPage  navigate={useNavigate()} />}/>
     </Routes>
-// New routes go here
+// New routes go inside <Routes> tags
 );
 }
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -21,29 +21,37 @@ const Login = () => {
     }
 
     return(
-        <div className = "container">
-            <div className = "row justify-content-center">
-                <form className = "col-md-4 mt-3 pt-3 pb-3">
-                    { "" !== notice &&
-                        <div className = "alert alert-warning" role = "alert">
-                            { notice }    
-                        </div>
-                    }                  
-                    <div className = "form-floating mb-3">
-                        <input type = "email" className = "form-control" id = "exampleInputEmail1" aria-describedby = "emailHelp" placeholder = "name@example.com" value = { email } onChange = { (e) => setEmail(e.target.value) }></input>
-                        <label htmlFor = "exampleInputEmail1" className = "form-label">Email address</label>
+
+        <div className = "container-fluid">
+            <div className = "row justify-content-center mt-3">
+                <div className = "col-md-4 text-center">
+                    <p className = "lead">Log in</p>
+                </div>
+                <div className = "container">
+                    <div className = "row justify-content-center">
+                        <form className = "col-md-4 mt-3 pt-3 pb-3">
+                            { "" !== notice &&
+                                <div className = "alert alert-warning" role = "alert">
+                                    { notice }    
+                                </div>
+                            }                  
+                            <div className = "form-floating mb-3">
+                                <input type = "email" className = "form-control" id = "exampleInputEmail1" aria-describedby = "emailHelp" placeholder = "name@example.com" value = { email } onChange = { (e) => setEmail(e.target.value) }></input>
+                                <label htmlFor = "exampleInputEmail1" className = "form-label">Email address</label>
+                            </div>
+                            <div className = "form-floating mb-3">
+                                <input type = "password" className = "form-control" id = "exampleInputPassword1" placeholder = "Password" value = { password } onChange = { (e) => setPassword(e.target.value) }></input>
+                                <label htmlFor = "exampleInputPassword1" className = "form-label">Password</label>
+                            </div>
+                            <div className = "d-grid">
+                                <button type = "submit" className = "btn btn-primary pt-3 pb-3" onClick = {(e) => loginWithUsernameAndPassword(e)}>Submit</button>
+                            </div>
+                            <div className = "mt-3 text-center">
+                                <span>Need to sign up for an account? <Link to = "/signup">Click here.</Link></span>
+                            </div>
+                        </form>
                     </div>
-                    <div className = "form-floating mb-3">
-                        <input type = "password" className = "form-control" id = "exampleInputPassword1" placeholder = "Password" value = { password } onChange = { (e) => setPassword(e.target.value) }></input>
-                        <label htmlFor = "exampleInputPassword1" className = "form-label">Password</label>
-                    </div>
-                    <div className = "d-grid">
-                        <button type = "submit" className = "btn btn-primary pt-3 pb-3" onClick = {(e) => loginWithUsernameAndPassword(e)}>Submit</button>
-                    </div>
-                    <div className = "mt-3 text-center">
-                        <span>Need to sign up for an account? <Link to = "./signup">Click here.</Link></span>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     )
