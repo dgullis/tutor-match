61b6ee0282d28093cd5ca412c672bab20d4735f9
simonbudden <simon.budden@gmail.com>
2024-02-28 15:17:12 +0000

    Initial commit, firebase admin authentication added + passing test, location of gitignore changed and .env added to it

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,14 @@ from flask_cors import CORS, cross_origin
 from datetime import datetime, timezone
 from bson import BSON
 import json
+from firebase_admin import credentials, initialise_app
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
+cred = credentials.Certificate('firebaseServiceAccountKey.json')
+firebase_admin = initialize_app(cred)
 
 if __name__ == '__main__':
     app.run(debug=True)
