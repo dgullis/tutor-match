b492b75b5e15dcfd7c053b06dd249e51d4f2f473
dan gullis <daniel.gullis1@gmail.com>
2024-02-28 10:02:44 +0000

    basic navbar setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
-import { useNavigate, Routes, Route } from "react-router-dom";
+import { useNavigate, Routes, Route, Link } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Layout from "./components/Layout";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Profile from "./components/Profile";
+import {Navbar, Nav, Container} from 'react-bootstrap'
 
 function App() {
   return (
+    <>
+    <Navbar bg="primary" data-bs-theme="dark">
+      <Container>
+      <Navbar.Brand>TutorMatch</Navbar.Brand>
+      <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/">Login</Nav.Link>
+          <Nav.Link as={Link} to="/signup">SignUp</Nav.Link>
+      </Nav>
+      </Container>
+    </Navbar>
+
+
     <Routes>
       <Route path = "/" element = { <Layout></Layout> }>
             <Route index element = { <Login></Login> }></Route>
@@ -15,7 +29,9 @@ function App() {
           </Route>
       <Route path='/landing' element={<LandingPage  navigate={useNavigate()} />}/>
     </Routes>
-// New routes go here
+
+    </>
+
 );
 }
 
