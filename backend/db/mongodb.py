024a73e86a83281e24cf31d3ece5829d446961bb
“Leah” <lnsimonn@hotmail.com>
2024-02-29 16:11:55 +0000

    fix connection to db - just need to pass in .env file

--- backend/db/mongodb.py
+++ backend/db/mongodb.py
@@ -5,13 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 db_name= os.getenv('DB_NAME')
 
-mongoClient = MongoClient('mongodb://127.0.0.1:27017')
+#mongodb_url = os.getenv("MONGODB_URL")
+mongoClient = MongoClient('MONGOURL')
+#mongoClient = MongoClient('')
+print(db_name, "HELLLLLOOOO")
+
 db = mongoClient.get_database(db_name)
 
 users_collection = db.get_collection('users')
 subjects_collection = db.get_collection('subjects')
 bookings_collection = db.get_collection('bookings')
-
 
 def get_users_collection():
     return users_collection
@@ -21,3 +24,4 @@ def get_subjects_collection():
 
 def get_bookings_collection():
     return bookings_collection
+    
