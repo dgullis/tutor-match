bd102c6335c3a786efd92a9a137b444e0b55d6c4
Ann Galloway <60460582+AnnGalloway@users.noreply.github.com>
2024-02-29 17:54:22 +0000

    subject and grade can be added

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { auth } from "../firebase";
 import { useAuth } from "../components/authContext";
 import { getUser } from "../services/users";
+import { addSubject } from "../services/subjects";
 
 
 const Profile = () => {
@@ -11,6 +12,9 @@ const Profile = () => {
     const handle = useParams()
     const firebase_id = handle.id
     const [userDetails, setUserDetails] = useState({})
+    const [subject, setSubject] = useState("")
+    const [grade, setGrade] = useState("")
+
 
     useEffect(() => {
         getUser(firebase_id)
@@ -23,19 +27,58 @@ const Profile = () => {
             });
     },[]);
 
-    const logoutUser = async (e) => {
+    const addSubjectAndLevel = async (e) => {
         e.preventDefault();
 
-        await auth.signOut();
-        navigate("/");
-    }
+        try {
+            await addSubject(subject, grade, firebase_id);
+            navigate(0)
+    } catch(error) {
+        console.log(error)
+    }}
 
     return(
         <>
+        <div>
         <h2>Tutor Details</h2>
         <p>Name: {userDetails.name}<br/>
         Email: {userDetails.email}<br/>
         Subjects: Maths - GCSE, Science - A Level</p>
+        </div>
+        <div>
+            <div className = "container-fluid">
+            <div className = "row justify-content-center mt-3">
+                <div className = "col-md-4 text-center">
+                    <p className = "lead">Add subjects</p>
+                </div>
+                <div className = "container">
+                    <div className = "row justify-content-center">
+                        <form className = "col-md-4 mt-3 pt-3 pb-3">
+                        <select className="form-select" aria-label="Default select example" value = {subject} onChange={ (e) => setSubject(e.target.value)}>
+                            <option defaultValue>Select your subject</option>
+                            <option value="English">English</option>
+                            <option value="Maths">Maths</option>
+                            <option value="Science">Science</option>
+                            </select>
+                        <div className="custom-control custom-radio">
+                        <input type="radio" id="customRadio1" name="customRadio" className="custom-control-input" value = {"gcse"} onChange = { (e) => setGrade(e.target.value)}></input>
+                        <label className="custom-control-label" htmlFor="customRadio1">GCSE</label>
+                        </div>
+                        <div className="custom-control custom-radio">
+                        <input type="radio" id="customRadio2" name="customRadio" className="custom-control-input" value = {"alevel"} onChange = { (e) => setGrade(e.target.value)}></input>
+                        <label className="custom-control-label" htmlFor="customRadio2">A Level</label>
+                        </div>
+
+
+                            <div className = "d-grid">
+                                <button type = "submit" className = "btn btn-primary pt-3 pb-3" onClick = {(e) => addSubjectAndLevel(e)}>Sign up</button>
+                            </div>                   
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
         </>
     )    
 }
