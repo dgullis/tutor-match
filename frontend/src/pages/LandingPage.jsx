edca1ee99f1c6adf33da024560611b13932d56bf
KittyKat <140555163+KatBiel@users.noreply.github.com>
2024-02-29 10:59:07 +0000

    logo and initial landing page styling added

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,27 @@
+import React from 'react';
+
+// container, row, and col-md-6 classes from Bootstrap to create a responsive layout with two columns.
+
 const LandingPage = () => {
     return (
-        <h1>Hello!</h1>
-
-    )
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <h1>Welcome to Our Website!</h1>
+                    <p>This is a Makers Academy final project using FReMP tech stack by</p>
+                    <ul className="list-unstyled">
+                        <li>John Doe</li>
+                        <li>Bob Marley</li>
+                        <li>Alice in Wonderland</li>
+                    </ul>
+                    <button className="btn btn-primary">Create a free account</button>
+                </div>
+                <div className="col-md-6">
+                    <img src="/images/logo.png" alt="Logo" className="img-fluid" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default LandingPage;
