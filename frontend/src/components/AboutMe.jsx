99ec7de7109e7842c7a6305a95e69ac090133ced
“Leah” <lnsimonn@hotmail.com>
2024-03-05 12:27:58 +0000

    frontend implement add bio using state

--- frontend/src/components/AboutMe.jsx
+++ frontend/src/components/AboutMe.jsx
@@ -1,27 +1,69 @@
 import React from 'react';
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
 
 const AboutMe = ({ userDetails }) => {
-  return (
-    <>
-       {/* Bios for Students/Tutors*/}
-       <div>
-            {userDetails.status === "Tutor" && (
-                <div>
-                    <h2>Tutor Bio</h2>
-                    {/* Tutor */}
-                </div>
-            )}
 
-            {userDetails.status === "Student" && (
-                <div>
-                    <h2>Student Bio</h2>
-                    {/* About Students */}
-                </div>
+    //USESTATE
+    const [bio, setBio] = useState("");
+    const [Editing, setIsEditing] = useState(false)
+
+    //HANDLES CHANGE BIO
+    const handleChangeBio = (e) => {
+        console.log(e.target.value);
+        setBio(e.target.value);
+      };
+
+    //HANDLES SUBMIT
+    const handleSubmit = (e) => {
+       e.preventDefault();
+       console.log("Updated Bio:", bio)
+       setIsEditing(false);
+      };
+      return (
+        <div>
+            {/* Bios for Students/Tutors */}
+            <div>
+                {userDetails.status === "Tutor" && (
+                    <div>
+                        <h2>Tutor Bio</h2>
+                    </div>
+                )}
+
+                {userDetails.status === "Student" && (
+                    <div>
+                        <h2>Student Bio</h2> 
+                    </div>
+                )}
+            </div>
+            
+            {/* Updating Bio */}
+            {Editing ? (
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="bioTextArea">
+                        <Form.Label>Bio</Form.Label>
+                        <Form.Control
+                            as="textarea"
+                            rows={3}
+                            value={bio}
+                            onChange={handleChangeBio}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Save
+                    </Button>
+                </Form>
+            ) : (
+                <>
+                    <p>{bio}</p>
+                    <Button variant="success" onClick={() => setIsEditing(true)}>
+                        Edit Bio
+                    </Button>
+                </>
             )}
         </div>
-    </>
-  );
+    );
 };
 
 export default AboutMe;
-
