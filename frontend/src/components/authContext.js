263294cbbd146a2c277c6af7ab2099a0a91196c0
simonbudden <simon.budden@gmail.com>
2024-03-05 08:49:30 +0000

    Firebase auth fixed, getIdToke n re-added to authContext

--- frontend/src/components/authContext.js
+++ frontend/src/components/authContext.js
@@ -70,32 +70,37 @@ export const AuthProvider = ({ children }) => {
 
     // sets up listener to observe changes in users authentication state
     // refreshed state of user and mongoUSer
-   useEffect(() => {
-          const unsubscribe = auth.onAuthStateChanged(async (user)=> {
+    useEffect(() => {
+            const unsubscribe = auth.onAuthStateChanged(async (user)=> {
 
-              if(user){
-                  setUser(user)
-                  try {
-                      const firebase_id = user.uid;
-                      const result = await getUser(firebase_id);
-                      setMongoUser(result.user);
-                  } catch (error) {
-                      console.log("error refreshing userdetails from mongoDB ", error)
-                  } 
-              } else {
-                  setUser(null)
-                  setMongoUser(null)
-                  setIsFetching(false)
-                  return
-              }
+                if(user){
+                    setUser(user)
+                    user.getIdToken().then(function(idToken) {
+                        setIdToken(idToken)
+                    }).catch(error => {
+                        console.error("Error fetching data: ", error);
+                    })
+                    try {
+                        const firebase_id = user.uid;
+                        const result = await getUser(firebase_id, idToken);
+                        setMongoUser(result.user);
+                    } catch (error) {
+                        console.log("error refreshing userdetails from mongoDB ", error)
+                    } 
+                } else {
+                    setUser(null)
+                    setMongoUser(null)
+                    setIsFetching(false)
+                    return
+                }
 
-              setIsFetching(false)
+                setIsFetching(false)
 
 
-          })
-          // unsubcribe is a cleaunup function ensures listener is detached when the componenet is not in use
-          return () => unsubscribe()
-      }, []);
+            })
+            // unsubcribe is a cleaunup function ensures listener is detached when the componenet is not in use
+            return () => unsubscribe()
+        }, []);
 
     if (isFetching) {
         return (
