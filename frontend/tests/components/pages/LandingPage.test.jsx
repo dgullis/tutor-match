9cbcba9a3289120d2c19d5b723232aed82543773
KittyKat <140555163+KatBiel@users.noreply.github.com>
2024-03-05 09:24:37 +0000

    Landing page test

--- frontend/tests/components/pages/LandingPage.test.jsx
+++ frontend/tests/components/pages/LandingPage.test.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import LandingPage from '../../../src/pages/LandingPage';
 
-test("renders LandingPage component", async () => {
-  render(
-    <BrowserRouter>
-      <LandingPage />
-    </BrowserRouter>
-  );
-  expect(screen.getByText(/Online tutoring that releases potential/i)).toBeTruthy();
+describe("Landing Page", () => {
+  test("renders LandingPage component", async () => {
+    render(
+      <BrowserRouter>
+        <LandingPage />
+      </BrowserRouter>
+    );
+    expect(screen.getByText(/Online tutoring that releases potential/i)).toBeTruthy();
+  });
 });
+  
