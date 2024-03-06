c840129fa21d6029af0e64aa07d6294ce48eba5b
Dan Dullis <daniel.gullis1@gmail.com>
2024-03-06 11:17:16 +0000

    click to see reviews

--- frontend/src/components/TutorRating/TutorStarRating.jsx
+++ frontend/src/components/TutorRating/TutorStarRating.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
+import { Modal, Button } from 'react-bootstrap'
 
 export const TutorStarRating = ({tutorReviews, tutorRating}) => {
     const [avgRating, setAvgRating] = useState(null)
     const [stars, setStars] = useState([])
     const [numRatings, setNumRatings] = useState(null)
+    const [showReviews, setShowReviews] = useState(false)
+
+    const handleClick = () => setShowReviews(true);
+    const handleClose = () => setShowReviews(false)
+    
 
     useEffect(() => {
         setAvgRating(Math.round(tutorRating))
@@ -19,22 +25,46 @@ export const TutorStarRating = ({tutorReviews, tutorRating}) => {
 
     return (
         <>
-        numRatings > 0 ? 
-        <div className="container text-center">
-            <span style={{ fontSize: '14px' }}>({avgRating})</span>
-            {stars}
-            <span style={{ fontSize: '14px' }}>{numRatings}{numRatings > 1 ? ' reviews' : ' review'}</span>
-        </div>
+        {numRatings > 0 ? (
 
-        :
+            <div className="container text-center">
+                <span style={{ fontSize: '14px' }}>({avgRating})</span>
+                {stars}
+                <span style={{ fontSize: '14px' }}>{numRatings}{numRatings > 1 ? ' reviews' : ' review'}</span>
+            
 
-        <div className="container text-center">
-            <span style={{ fontSize: '14px' }}>No reviews</span>
-        </div>
+                <div onClick={handleClick} style={{ color: 'blue', textDecoration: 'underline', cursor: 'pointer' }}>
+                    see reviews
+                </div>
 
-        <p>see</p>
+            </div>
+
+        ) : (
+
+            <div className="container text-center">
+                <span style={{ fontSize: '14px' }}>No reviews</span>
+            </div>
+        
+        )}
+        
+
+        <Modal show={showReviews} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Reviews</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {tutorReviews.map((review) => {
+                    return <p>"{review.comment}"</p>
+                })}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+            </Modal.Footer> 
+        </Modal>
+
         </>
-
-
     )
+
 }
