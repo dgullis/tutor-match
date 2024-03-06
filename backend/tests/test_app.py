8e26612d71fb310f2179aa3393235f35c4c2b697
“Leah” <lnsimonn@hotmail.com>
2024-03-06 10:10:02 +0000

    test: saeguarding

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -38,7 +38,7 @@ def test_signup(client):
         "name": "Test name",
         "email": "test@email.com",
         "status": "Student",
-        "safeguarding": None
+        "safeguarding": "Approved"
     }
     response = client.post("/signup", json=user)
     assert response.status_code == 201
