9b015c39d8e32e2d8f2296ce68f27f0c87001bb8
KittyKat <140555163+KatBiel@users.noreply.github.com>
2024-03-07 10:42:54 +0000

    Fixed bug with Tutor Search result PFP

--- frontend/src/components/TutorSearchResult.jsx
+++ frontend/src/components/TutorSearchResult.jsx
@@ -10,7 +10,7 @@ export const TutorSearchResult = ( { tutor }) => {
             <Card.Body>
             <Row>
             <Col md={4}>
-                <Card.Img style={{ width: "80px", height: "80px", borderRadius: "50%" }} variant="top" src={tutor.profileImage ? tutor.profileImage : "DEFAUhttps://firebasestorage.googleapis.com/v0/b/tutormatch-e2a6a.appspot.com/o/profile-images%2FScreenshot%202024-03-07%20at%2010.12.22.png?alt=media&token=22295bdd-8c61-4837-aba0-6bc98985c0f3"}/>
+                <Card.Img style={{ width: "80px", height: "80px", borderRadius: "50%" }} variant="top" src={tutor.profileImage ? tutor.profileImage : "https://firebasestorage.googleapis.com/v0/b/tutormatch-e2a6a.appspot.com/o/profile-images%2FScreenshot%202024-03-07%20at%2010.12.22.png?alt=media&token=22295bdd-8c61-4837-aba0-6bc98985c0f3"}/>
             </Col>
             <Col md={8} className="d-flex align-items-center">
                 <Link to={`/profile/${tutor.firebase_id}`} style={{color: 'hsl(0, 0%, 20%)', textDecoration: 'none'}}>
