6bc16c1d38766502b5bec5184e5bf18293eb02a9
Dan Dullis <daniel.gullis1@gmail.com>
2024-03-18 15:49:01 +0000

    setup for aws deployment

---
+++ frontend/src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage, ref } from "firebase/storage";
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FB_API_KEY,
+    authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FB_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FB_MESSAGING_ID,
+    appId: process.env.REACT_APP_FB_APP_ID,
+    measurementId: process.env.REACT_APP_FB_MEASURE_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const storage = getStorage();
+const storageRef = ref(storage)
+
+export { auth, storage, storageRef }
+
+
+
+
